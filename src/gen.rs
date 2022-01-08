9252a29c8449992c7a813913b0d176c1f1c636e1
Fraser Balch <farseer.ulwithy@gmail.com>
2022-01-08 14:21:18 -0700

    Refactor OrbitGenerator into Distribution

--- src/gen.rs
+++ src/gen.rs
@@ -1,15 +1,17 @@
 use crate::*;
 use rand::Rng;
+use std::cell::Cell;
 
 pub struct OrbitGenerator {
     pub stellar_mass: Mass,
-    pub semi_major_axis: Length,
+    pub semi_major_axis: Cell<Length>,
 }
 
-impl OrbitGenerator {
-    pub fn gen_next<R: Rng>(&mut self, rng: &mut R) -> EllipticalOrbit {
-        let semi_major_axis = self.semi_major_axis;
-        self.semi_major_axis *= rng.gen_range(1.3..2.0);
+impl Distribution<EllipticalOrbit> for OrbitGenerator {
+    fn sample<R: Rng + ?Sized>(&self, rng: &mut R) -> EllipticalOrbit {
+        let semi_major_axis = self.semi_major_axis.get();
+        self.semi_major_axis
+            .set(self.semi_major_axis.get() * rng.gen_range(1.3..2.0));
 
         EllipticalOrbit::new(
             self.stellar_mass,
@@ -18,5 +20,29 @@ impl OrbitGenerator {
             rng.gen(),
             rng.gen(),
         )
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use rand::thread_rng;
+
+    #[test]
+    fn each_orbit_larger_than_the_last() {
+        let generator = OrbitGenerator {
+            stellar_mass: Mass::in_kg(1.0),
+            semi_major_axis: Cell::new(Length::in_m(1.0)),
+        };
+
+        let rng = &mut thread_rng();
+
+        let mut orbit = generator.sample(rng);
+
+        for _ in 0..10 {
+            let next = generator.sample(rng);
+            assert!(next.semi_major_axis > orbit.semi_major_axis);
+            orbit = next;
+        }
     }
 }
